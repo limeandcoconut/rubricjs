1652a39714c7b29eba8d85d0d7952071834b3736
Jacob Smith <messagethesmith@gmail.com>
2016-12-03 10:31:25 -0800

    Remove errant test.js file

--- test.js
+++
@@ -1,6 +0,0 @@
-import './test/EntityFactoryTests.js';
-import './test/EntityManagerTests.js';
-import './test/InputAdapterTests.js';
-import './test/RubricTests.js';
-import './test/SystemTests.js';
-import './test/TimerTests.js';
